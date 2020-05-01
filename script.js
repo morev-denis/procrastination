ca8156c27ca40a05191c09a2cb3a6948c18828f0
Denis Morev <morev.denis@gmail.com>
2020-05-01 10:10:33 +0300

    поправить script.js

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function getRandomElement(arr) {
   let randIndex = Math.floor(Math.random() * arr.length);
   return arr[randIndex];
 }
+
+let button = document.querySelector('.button');
+let advice = document.querySelector('.advice');
+let phrase = document.querySelector('.phrase');
+let image = document.querySelector('.image');
+
+
+phrase.textContent = getRandomElement(phrases);
