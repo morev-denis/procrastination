50b52282237fe83f8655f58d17a32e66427ac8ea
Denis Morev <morev.denis@gmail.com>
2020-05-02 14:11:23 +0300

    поправить script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let phrases = [
-  'отправить другу смешную гифку',
-  'посмотреть скидки на авиабилеты',
-  'разобраться, о чём поют рэперы',
-  'Arzamas',
-  'расставить книги на полке по цвету',
-  'читать про зарплаты в Сан-Франциско'
+  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+  { text: 'Arzamas', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' },
 ];
 
 function getRandomElement(arr) {
@@ -13,9 +17,18 @@ function getRandomElement(arr) {
 }
 
 let button = document.querySelector('.button');
-let advice = document.querySelector('.advice');
 let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
 let image = document.querySelector('.image');
 
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(phrases);
+  phrase.textContent = randomElement.text;
+  image.src = randomElement.image;
 
-phrase.textContent = getRandomElement(phrases);
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
+  }
+});
